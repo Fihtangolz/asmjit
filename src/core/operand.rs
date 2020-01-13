e71cefab111a75f1c3e70ccd69a749299c1701ee
Dmitry <mamhigtt@gmail.com>
2020-01-13 15:44:40 +0700

    fix type method naming, transfer OpType

--- src/core/operand.rs
+++ src/core/operand.rs
@@ -1,6 +1,38 @@
 //! Count of register groups used by virtual registers.
 pub const K_GROUP_VIRT: usize = 4;
 
+/// Operand types that can be encoded in `Operand`.
+enum OpType {
+    // Not an operand or not initialized.
+    OpNone,
+    // Operand is a register.
+    Reg,
+    // Operand is a memory.
+    Mem,
+    // Operand is an immediate value.
+    Imm,
+    // Operand is a label.
+    Label,
+}
+
+/// Constructor-less `Operand`.
+///
+/// Contains no initialization code and can be used safely to define an array
+/// of operands that won't be initialized. This is an `Operand` compatible
+/// data structure designed to be statically initialized, static const, or to
+/// be used by the user to define an array of operands without having them
+/// default initialized.
+///
+/// The key difference between `Operand` and `Operand_`:
+///
+/// ```
+/// Operand_ xArray[10]; // Not initialized, contains garbage.
+/// Operand  yArray[10]; // All operands initialized to none.
+/// ```
+struct Operand_ {
+
+}
+
 pub struct Operand {
 
 }
@@ -17,7 +49,7 @@ pub struct BaseReg {
 trait RegInfo {
     fn operand_type();
     fn group();
-    fn type();
+    fn r#type();
     fn size();
 }
 
