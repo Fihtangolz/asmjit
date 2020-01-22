37411d6f6c85f85511abb702839e524e8900ddbd
Dmitry <mamhigtt@gmail.com>
2020-01-23 01:39:28 +0700

    add scopeguard craite

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 #![feature(stmt_expr_attributes)]
 #![feature(const_fn)]
 #![feature(asm)]
+#![feature(rustc_private)]
 
 #[macro_use]
 extern crate bitflags;
+
+#[macro_use(defer)] 
+extern crate scopeguard;
 
 mod core;
 mod x86;
