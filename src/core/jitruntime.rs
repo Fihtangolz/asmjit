e23b88b8cbc2571a0fc48dc49d5cbb2d91d518d6
Dmitry <mamhigtt@gmail.com>
2020-01-26 06:34:36 +0700

    add imports, fields, comment

--- src/core/jitruntime.rs
+++ src/core/jitruntime.rs
@@ -1,12 +1,14 @@
-struct JitRuntime {
-
-}
+use super::target::Target;
+use super::jitallocator::JitAllocator;
 
 /// JIT execution runtime is a special `Target` that is designed to store and
 /// execute the generated code.
-impl JitRuntime {
-    aloc: JitAllocator;
-    
+struct JitRuntime {
+    aloc: JitAllocator,
+    target: Target,
+}
+
+impl JitRuntime { 
     pub fn reset();
     pub fn aloc();
     pub fn add();
