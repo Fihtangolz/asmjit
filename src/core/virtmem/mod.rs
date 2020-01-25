fc5bae184dbb26b9417de91310a2f5e43c77ab8a
Dmitry <mamhigtt@gmail.com>
2020-01-25 09:06:18 +0700

    add public wrapper for adhoc platform function (it is also help research API), fix naming, correct use  derective

--- src/core/virtmem/mod.rs
+++ src/core/virtmem/mod.rs
@@ -1,29 +1,30 @@
-use std::sync::Once;
-
 #[cfg(target_family = "windows")]
 mod win;
 #[cfg(target_family = "windows")]
-use win::*;
+use win as target_impl;
 
 #[cfg(target_family = "unix")] 
 mod unix;
 #[cfg(target_family = "unix")] 
-use unix::*;
+use unix as target_impl;
+
+use std::sync::Once;
+use crate::core::globals::Error;
 
 bitflags! {
     /// Virtual memory and memory mapping flags.
     pub struct Flags: u32 {
         /// No access flags.
-        const AccessNone = 0x00000000;
+        const ACCESS_NONE = 0x00000000;
         /// Memory is readable.
-        const AccessRead = 0x00000001;
+        const ACCESS_READ = 0x00000001;
         /// Memory is writable (implies read access).
-        const AccessWrite = 0x00000002;
+        const ACCESS_WRITE = 0x00000002;
         /// Memory is executable (implies read access).
-        const AccessExecute = 0x00000004;
+        const ACCESS_EXECUTE = 0x00000004;
     
         /// A combination of `kAccessRead | kAccessWrite`
-        const AccessReadWrite = 0x00000003;
+        const ACCESS_READ_WRITE = 0x00000003;
     
         /// Not an access flag, only used by `allocDualMapping()` to override the
         /// default allocation strategy to always use a temporary directory instead
@@ -32,7 +33,7 @@ bitflags! {
         /// by a different API than `open()` or `shm_open()`. For example on Linux
         /// `memfd_create()` is preferred and on BSDs `shm_open(SHM_ANON, ...)` is
         /// used if SHM_ANON is defined.
-        const MappingPreferTmp = 0x80000000;
+        const MAPPING_PREFER_TMP = 0x80000000;
     }
 }
 
@@ -72,15 +73,59 @@ pub fn info() -> &'static Info {
     static INIT: Once = Once::new();
     unsafe {
         INIT.call_once(|| {
-            INFO = get_info();
+            INFO = target_impl::info();
         });
         &INFO
     }
 }
 
-pub enum ShmStrategy {
-    Unknown,
-    DevShm,
-    TmpDir,
+/// Allocates virtual memory by either using `VirtualAlloc()` (Windows)
+/// or `mmap()` (POSIX).
+///
+/// \note `size` should be aligned to a page size, use \ref VirtMem::info()
+/// to obtain it. Invalid size will not be corrected by the implementation
+/// and the allocation would not succeed in such case.
+pub unsafe fn alloc(size: usize, flags: Flags) -> Result<*mut u8, Error> {
+    target_impl::alloc(size, flags)
 }
+
+/// Releases virtual memory previously allocated by \ref  VirtMem::alloc() or
+/// \ref VirtMem::allocDualMapping().
+///
+/// \note The size must be the same as used by \ref VirtMem::alloc(). If the
+/// size is not the same value the call will fail on any POSIX system, but
+/// pass on Windows, because of the difference of the implementation.
+pub unsafe fn release(ptr: *mut u8, size: usize) -> Option<Error> {
+    target_impl::release(ptr, size)
+}
+
+/// A cross-platform wrapper around `mprotect()` (POSIX) and `VirtualProtect`
+/// (Windows).
+pub unsafe fn protect(ptr: *mut u8, size: usize, flag: Flags) -> Option<Error> {
+    target_impl::protect(ptr, size, flag)
+}
+
+impl DualMapping {
+    /// Allocates virtual memory and creates two views of it where the first view
+    /// has no write access. This is an addition to the API that should be used
+    /// in cases in which the operating system either enforces W^X security policy
+    /// or the application wants to use this policy by default to improve security
+    /// and prevent an accidental (or purposed) self-modifying code.
+    ///
+    /// The memory returned in the `dm` are two independent mappings of the same
+    /// shared memory region. You must use \ref VirtMem::releaseDualMapping() to
+    /// release it when it's no longer needed. Never use `VirtMem::release()` to
+    /// release the memory returned by `allocDualMapping()` as that would fail on
+    /// Windows.
+    pub unsafe fn alloc(size: usize, flags: Flags) -> Result<DualMapping, Error> {
+        target_impl::alloc_dual_mapping(size, flags)
+    }
+
+    /// Releases the virtual memory mapping previously allocated by
+    /// [alloc_dual_mapping](alloc_dual_mapping).
+    pub unsafe fn release(self, size: usize) -> Option<Error> {
+        target_impl::release_dual_mapping(self, size)
+    }
+}
+
 
